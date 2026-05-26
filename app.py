dea990735730127f6330bacacee5414131262d0d
jazhuillca <jazhuillcag@gmail.com>
2026-05-26 16:52:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,11 @@ if sesion == "Sesión 1":
 elif sesion == "Sesión 2":
   st.write("Bienvenido a la sesión 2")
 
+  precio = st.number_input("Ingrese el precio del producto")
+  descuento = st.number_input("Ingrese el descuento del producto")
+
+precio_final_producto = precio - (precio*descuento)
+
 elif sesion == "Sesión 3":
   st.write("Bienvenido a la sesión 3")
+  inicio_rango = st.slider("seleccione un valor",min_value = 0, max_value=20, value =7)

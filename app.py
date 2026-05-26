3f462f10d1109513be2f37cf8834b1ac3c355dd2
jazhuillca <jazhuillcag@gmail.com>
2026-05-26 16:21:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ st.title("Mi primera aplicación en python")
 
 st.sidebar.title("Parametros")
 
-st.write("Elaborado por: Carlos Carrillo")
+st.write("Elaborado por: Jazmin Huillca")
+
+sesion = st.selectbox("Seleccione una sesión"),["Sesion 1","Sesion 2", "Sesion 3", "Sesion 4"])
+
 

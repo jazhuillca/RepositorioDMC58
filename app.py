bff73758797e889072b7009cec94f05a49740973
jazhuillca <jazhuillcag@gmail.com>
2026-05-26 18:26:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ st.sidebar.image("DMC.png")
 sesion = st.sidebar.selectbox("Seleccione una sesión",["Sesión 1","Sesión 2", "Sesión 3", "Sesión 4"])
 
 if sesion == "Sesión 1":
-  st.write("Bienvenido a la sesión 1")
+  st.write("Bienvenido a la Sesión 1")
   st.image("Python_logo.png")
 
 elif sesion == "Sesión 2":
-  st.write("Bienvenido a la sesión 2")
+  st.write("Bienvenido a la Sesión 2")
 
   precio = st.number_input("Ingrese el precio del producto", min_value = 0 , max_value = 5000 , value =1200)
   descuento = st.number_input("Ingrese el descuento del producto del 0 al 100%", min_value = 0 , max_value = 100)
@@ -24,5 +24,5 @@ elif sesion == "Sesión 2":
   st.write("El precio final del producto es: ", precio_final_producto)
 
 elif sesion == "Sesión 3":
-  st.write("Bienvenido a la sesión 3")
+  st.write("Bienvenido a la Sesión 3")
   inicio_rango = st.slider("seleccione un valor",min_value = 0, max_value=20, value =7)

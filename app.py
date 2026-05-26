ce259a6533e578529cb4a3bd311577548d514fc1
jazhuillca <jazhuillcag@gmail.com>
2026-05-26 16:33:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ st.sidebar.title("Parametros")
 
 st.write("Elaborado por: Jazmin Huillca")
 
+st.sidebar.image("DMC.png")
+
 sesion = st.sidebar.selectbox("Seleccione una sesión",["Sesion 1","Sesion 2", "Sesion 3", "Sesion 4"])
 
+if sesion == "Sesión 1":
+  st.write("Bienvenido a la sesión 1")
+  st.image("Python_logo.png")
 
+elif sesion == "Sesión 2":
+  st.write("Bienvenido a la sesión 2")
+
+elif sesion == "Sesión 3":
+  st.write("Bienvenido a la sesión 3")

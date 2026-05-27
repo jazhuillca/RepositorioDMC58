151ba3847d5aed1669a32b1a413a0766c6b182a8
jazhuillca <jazhuillcag@gmail.com>
2026-05-26 20:01:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-import librerias_funciones as lf
 
 st.title("Mi primera aplicación en python")
 
@@ -26,11 +24,20 @@ elif sesion == "Sesión 2":
   st.write("Bienvenido a la Sesión 2")
 
   precio = st.number_input("Ingrese el precio del producto", min_value = 0 , max_value = 5000 , value =1200)
-  descuento = st.number_input("Ingrese el descuento del producto del 0 al 100%", min_value = 0 , max_value = 100)
 
-  precio_final_producto = precio - (precio*(descuento/100))
-  st.write("El precio final del producto es: ", precio_final_producto)
+  elif sesion == "Sesión 3":
 
+  st.write("Bienvenido la sesión 3")
+
+  fin_rango = st.slider("Selecione un valor",min_value = 0 , max_value=20, value =7 )
+
+  arreglo = np.arange(0 , fin_rango)
+
+  st.write(arreglo)
+
+else:
+
+  st.write("Bienvenido la sesión 4")
 elif sesion == "Sesión 3":
   st.write("Bienvenido a la Sesión 3")
   inicio_rango = st.slider("seleccione un valor",min_value = 0, max_value=20, value =7)

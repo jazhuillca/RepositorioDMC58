d8ef7a4779be6420da4becdd829dc556e85e2801
jazhuillca <jazhuillcag@gmail.com>
2026-05-26 20:10:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,3 +38,19 @@ elif sesion == "Sesión 3":
 else:
 
   st.write("Bienvenido la sesión 4")
+
+elif sesion == "Sesión 4":
+
+   st.write("Bienvenido a la sesión 4")
+
+   principal = st.number_input("Ingrese el monto del préstamo", value=1000)
+
+   tasa_anual = st.number_input("Ingrese la tasa anual en decimal", value=0.1, min_value=0.0, max_value=1.0)
+
+   anios = st.number_input("Ingrese el número de años del préstamo", value=1)
+
+   pagos_anio = st.number_input("Ingrese la cantidad de pagos por año", value=12)
+
+   cuota = round(lf.cuota_prestamo(principal, tasa_anual, anios, pagos_anio),2)
+
+   st.write(f"El valor de la cuota es {cuota}")
